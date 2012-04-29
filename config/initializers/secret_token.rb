506cbc2dabc52ff2e8d7c5fa2b51a78af2a65e3e
Ben Downey <BD@BenDowney.net>
2012-04-29 11:14:37 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChiRubyShopsApp::Application.config.secret_token = '10ac60761449fcefa5138b70f9d191d57c292c85c5155a625a2e5724b0cb984af01db069f05539c3ccf3efcd81159bad44ae424fd1c47e525d6d8a76901fd37f'
