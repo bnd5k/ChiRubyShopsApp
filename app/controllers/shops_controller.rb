64595a01a8283952af21039f3e6e7116ac1c94ab
Ben Downey <BD@BenDowney.net>
2012-05-03 10:39:50 -0500

    changed routes for exercise on routes

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -16,6 +16,7 @@ class ShopsController < ApplicationController
     @shop = Shop.new
     if shop.save
       flash[:success] = "Added"
+      redirect_to shops_url
     else
       redirect_to new
     end
@@ -29,6 +30,7 @@ class ShopsController < ApplicationController
     @shop = Shop.find_by_id(params[:id])
     if @shop.update_attributes(params[:shop])
       flash[:success] = "Info Updated"
+      redirect_to shop_url
     else
       render 'edit'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 ChiRubyShopsApp::Application.routes.draw do
   
-  resources :shops, :users, :ratings
-  root :to => 'shops#index'
-  match ':controller(/:action(/:id))(.:format)'
+ # resources :shops, :users, :ratings
+ # root :to => 'shops#index'
+ # match ':controller(/:action(/:id))(.:format)'
 
+ get '/shops', :controller => 'shops', :action => 'index', :as =>  :shops
+ get '/shops/:id', :controller => 'shops', :action => 'show', :as => :shop
+ 
+ get '/shops', :controller => 'shops', :action => 'new', :as => :new_shop
+ post '/shops', :controller => 'shops', :action => 'create'
+
+ get '/shops/:id/edit', :controller => 'shops', :action => 'edit', :as => :edit_shop 
+ put '/shops/:id/', :controller => 'shops', :action => 'update'
+
+ delete 'shops/:id', :controller => 'shops', :action => 'destroy'
+ 
+ 
+ root :to =>'shops#index'
 end
